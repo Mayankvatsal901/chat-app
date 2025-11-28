ab8f4c355acf2847a78ad334ca6d597ebe09c2a6
tutorial <mayankvatsal800@gmail.com>
2025-11-28 19:11:38 +0530

    yes

--- backend/src/index.cjs
+++ backend/src/index.cjs
@@ -29,7 +29,7 @@ app.use("/api/message",messageroutes)
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(PROJECT_ROOT,"frontend/dist")))
 // request is coming on any route then the entry point will be the index.html 
-  app.get("*",(req,res)=>{
+  app.get("/*",(req,res)=>{
     res.sendFile(path.join(PROJECT_ROOT,"frontend","dist","index.html"));
   })
 }
